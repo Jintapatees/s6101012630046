3b9dbab6076010b6fda078117243e5daf98b1965
jintapatee <jintapatees01@gmail.com>
2020-02-27 21:16:19 +0700

    Create Calculator_GET application

--- Calculate/models.py
+++ Calculate/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class History(models.Model):
+    x = models.IntegerField(null=True)
+    y = models.IntegerField(null=True)
+    operator = models.CharField(max_length=2000,null=True)
+    result = models.IntegerField(null=True)
+    def __str__(self):
+        return self.x
---
+++ Calculator_GET/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CalculatorGetConfig(AppConfig):
+    name = 'Calculator_GET'
