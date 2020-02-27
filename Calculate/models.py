d9ab61d7bb4e58fead88da65b1f098274d63ec0a
jintapatee <jintapatees01@gmail.com>
2020-02-27 22:33:45 +0700

    Can't link model object to views.py

--- Calculate/models.py
+++ Calculate/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class History(models.Model):
     x = models.CharField(max_length=20, null=True)
--- Calculate/views.py
+++ Calculate/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import History
 
 # Create your views here.
 def home(request):
@@ -6,14 +7,26 @@ def home(request):
 
 def calculate(request):
     if request.method == 'POST' :
-        input_1 = int(request.POST['input_1'])
-        input_2 = int(request.POST['input_2'])
+        input_1 = float(request.POST['input_1'])
+        input_2 = float(request.POST['input_2'])
+        History.object.create(x = input_1, y = input_2)
         if 'add' in request.POST:
             result = input_1 + input_2
+            op = '+'
+            History.object.create(operator = op)
         elif 'sub' in request.POST:
             result = input_1 - input_2
+            op = '-'
+            History.object.create(operator = op)
         elif 'multiple' in request.POST:
             result = input_1 * input_2
+            op = '*'
+            History.object.create(operator = op)
         elif 'divide' in request.POST:
             result = input_1 / input_2
-    return render(request, 'home.html',{'result':result})
+            op = '/'
+            History.object.create(operator = op)
+
+        History.object.create(result = result)
+        history_calculate = History.objects.all()
+    return render(request, 'home.html',{'result':result,'history_calculate':history_calculate})
